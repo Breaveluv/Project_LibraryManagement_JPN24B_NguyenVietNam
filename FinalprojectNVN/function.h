d8216f5d37f88351f4a9b691215d1ec4cf471822
Breaveluv <nam.bule12@gmail.com>
2025-01-06 22:17:55 +0700

    Add files via upload

---
+++ FinalprojectNVN/function.h
@@ -0,0 +1,14 @@
+//Noi trien khai cac nguyen mau ham
+#include "datatype.h"
+
+void loginAccount();
+
+void menu();
+
+void loadFile();
+void addBook();
+void editBook();
+void displayBook();
+void deleteBook();
+void sortBook();
+void searchBook();
